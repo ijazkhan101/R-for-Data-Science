e86a4201d1ad26bfa2c30723fa142fdce2e5bd8d
ijazdev101 <ijazkhan19932@gmail.com>
2023-02-24 21:30:27 +0500

    R
    R

--- firstday_r_programming/first_dayof_r.R
+++ firstday_r_programming/first_dayof_r.R
@@ -4,5 +4,22 @@
 # integer 
 
 # how to install packages
-setRepositories()
+install.packages("ggplot2" ,dependencies = TRUE)
+
+
+#import Data 
+
+data("iris")
+view(iris)
+
+library(ggplot2)
+
+
+ggplot(data=iris , mapping = aes( x= Sepal.Length ,y =Sepal.Width ))+geom_point()
+
+
+ggplot(data=iris , mapping = aes( x= Sepal.Length ,y =Sepal.Width , col = iris$Species))+geom_point()
+
+
+
 
